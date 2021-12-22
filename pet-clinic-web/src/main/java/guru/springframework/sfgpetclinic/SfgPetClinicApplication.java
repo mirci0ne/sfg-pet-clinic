0b5671c9d40f8a04b261b3d9a494dfdceaa135ce
Mircea Manole <mircea1406@gmail.com>
2021-12-22 21:55:10 +0200

    test

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClinicApplication.class, args);
+		 SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
 }
